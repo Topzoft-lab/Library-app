2fb32b10b9aed8ba734fdb7307789d7299e2b319
personal <omiwoletemitope@gmail.com>
2023-12-06 21:35:27 +0100

    setting up the app route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
 import "./App.css";
 import { Navbar } from "./layouts/Navbar";
-
+import { Redirect, Route, Switch } from "react-router-dom";
 import { Footer } from "./layouts/Footer";
 import { Homepage } from "./layouts/Homepage";
 import { SearchBookPage } from "./layouts/SearchBookPage";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      {/* <Homepage /> */}
-      <SearchBookPage />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            {" "}
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            {" "}
+            <Homepage />
+          </Route>
+          <Route path="/search">
+            <SearchBookPage />
+          </Route>
+        </Switch>
+      </div>
+
       <Footer />
     </div>
   );
