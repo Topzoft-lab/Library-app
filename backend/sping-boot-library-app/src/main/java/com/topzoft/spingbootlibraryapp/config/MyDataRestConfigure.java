b87a9e04e78ecb9238e795111ac0c9808011f740
personal <omiwoletemitope@gmail.com>
2023-12-22 12:51:09 +0100

    added review repository

--- backend/sping-boot-library-app/src/main/java/com/topzoft/spingbootlibraryapp/config/MyDataRestConfigure.java
+++ backend/sping-boot-library-app/src/main/java/com/topzoft/spingbootlibraryapp/config/MyDataRestConfigure.java
@@ -7,19 +7,22 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.topzoft.spingbootlibraryapp.entity.Book;
+import com.topzoft.spingbootlibraryapp.entity.Review;
 
 @Configuration
 public class MyDataRestConfigure implements RepositoryRestConfigurer {
-	public String theAllowedOrigin ="http://localhost:3000";
+	public String theAllowedOrigin ="http://localhost:3001";
 	
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		HttpMethod [] theUnsupportedActions = {HttpMethod.DELETE,HttpMethod.POST,HttpMethod.PATCH} ;
 		
 		config.exposeIdsFor(Book.class);
+		config.exposeIdsFor(Review.class);
 		
 		disableHttpMethods(Book.class,config,theUnsupportedActions);
 		
+		disableHttpMethods(Review.class,config,theUnsupportedActions);
 		/* Configure CORS mapping */
 		
 		cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigin);
